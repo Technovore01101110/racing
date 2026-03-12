4e8396c85d93f1ff125468f0464d490f2eeb02b1
nefiserrano <nefi_3902@hotmail.com>
2026-03-11 18:25:00 -0600

    Footer done

--- src/scripts/components/footer.js
+++ src/scripts/components/footer.js
@@ -1,9 +1,13 @@
+const today = new Date();
+const year = today.getFullYear();
+
 function footer() {
 	return `
       <div class="social">
-        <a target="_blank" href="https://www.instagram.com/greatwesternracing/#"><img src="" alt="Instagram"/></a>
-      	<a target="_blank" href="https://www.linkedin.com/company/great-western-racing/"><img src="" alt="LinkedIn"/></a>
+        <a target="_blank" href="https://www.instagram.com/greatwesternracing/#"><img src="./images/instagram-icon.svg" alt="Instagram icon"/></a>
+      	<a target="_blank" href="https://www.linkedin.com/company/great-western-racing/"><img src="./images/linkedin-161-svgrepo-com.svg" alt="LinkedIn icon"/></a>
       </div>
+      <p class="copyright">© ${year} Great Western Racing</p>
   `;
 }
 
